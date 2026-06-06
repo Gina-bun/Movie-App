a74b51657d75e33dc3dc1140cffea15ec1617ad1
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-06-06 10:46:19 +0000

    chore: add utils

--- src/services/api_service.ts
+++ src/services/api_service.ts
@@ -11,3 +11,11 @@ export const getMovies = async (genreId: number) => {
     return response.data.results
 }
 
+//to search movies
+export const searchMovies = async (searchedMovie) => {
+    const response = await axios.get(`${BASE_URL}/search/multi`, {
+        params: {api_key: API_KEY, query: searchedMovie}
+    })
+
+    return response.data.results
+}
